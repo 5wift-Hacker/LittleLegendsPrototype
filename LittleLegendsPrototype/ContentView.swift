c77ebeaefba5c8c09113d91d86d2e2f31ebcb6a8
John Newman <john.newman920@gmail.com>
2025-07-09 23:41:11 +1000

    prototyped screen built

--- LittleLegendsPrototype/ContentView.swift
+++ LittleLegendsPrototype/ContentView.swift
@@ -8,15 +8,146 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var correctAnswer: String = "Happiness"
+    
+    //if is correct, have right image, else have wrong image
+    @State var isCorrect: Bool = true
+    
+    @State var isVisible: Bool = false
+    
+    let rightImage: String = "checkmark"
+    let wrongImage: String = "xmark"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.ignoresSafeArea()
+            VStack(alignment: .leading) {
+                Image("john")
+                    .resizable()
+                    .scaledToFit()
+                    .border(Color.white, width: 10)
+                    .foregroundStyle(.tint)
+                Text("John's Story! Pt. 1")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.bottom)
+                Text("Once upon a time, John drove up a mountain to see an incredible sunset.")
+                    .font(.title3)
+                    .foregroundStyle(.white)
+                    .padding(.bottom)
+                Text("This is the start of his journey though!")
+                    .font(.title3)
+                    .foregroundStyle(.white)
+                    .padding(.bottom)
+                Text("What do you think John is feeling right now?")
+                    .font(.title3)
+                    .foregroundStyle(.white)
+                Spacer()
+            }
+            .padding()
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(height: 50)
+                                .frame(maxWidth: .infinity)
+                            Text("Happiness ☺️")
+                                .foregroundStyle(.white)
+                                .font(.headline)
+                        }
+                    }
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .strokeBorder(isCorrect ? Color.green : Color.red, lineWidth: 3)
+                            .frame(width: 50, height: 50)
+                        Image(systemName: isCorrect ? rightImage : wrongImage)
+                            .foregroundStyle(.white)
+                            .font(.largeTitle)
+                    }
+                }
+                
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(height: 50)
+                                .frame(maxWidth: .infinity)
+                            Text("Sadness 🥺")
+                                .foregroundStyle(.white)
+                                .font(.headline)
+                        }
+                    }
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .strokeBorder(isCorrect ? Color.green : Color.red, lineWidth: 3)
+                            .frame(width: 50, height: 50)
+                        Image(systemName: isCorrect ? rightImage : wrongImage)
+                            .foregroundStyle(.white)
+                            .font(.largeTitle)
+                    }
+                }
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(height: 50)
+                                .frame(maxWidth: .infinity)
+                            Text("Anger 😡")
+                                .foregroundStyle(.white)
+                                .font(.headline)
+                        }
+                    }
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .strokeBorder(isCorrect ? Color.green : Color.red, lineWidth: 3)
+                            .frame(width: 50, height: 50)
+                        Image(systemName: isCorrect ? rightImage : wrongImage)
+                            .foregroundStyle(.white)
+                            .font(.largeTitle)
+                    }
+                }
+                Button {
+                    
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(height: 50)
+                            .frame(maxWidth: .infinity)
+                        HStack {
+                            Text("Next")
+                                .foregroundStyle(.white)
+                                .font(.headline)
+                            Image(systemName: "arrow.right")
+                                .foregroundStyle(.white)
+                                .imageScale(.large)
+                        }
+                    }
+                }
+
+            }
+            
+            .padding()
         }
-        .padding()
-        .padding()
+    }
+    
+    //func check if correct answer
+    
+    func checkAnswer() {
+        
     }
 }
 
